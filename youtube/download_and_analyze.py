c0b243dbb058d457cdbe2eba84a23f8916aa20e5
mm1ladd-g <m.ghavampoori@icloud.com>
2024-06-15 00:54:23 +0330

    UPDATE: Added 5 more Youtube pages

--- youtube/download_and_analyze.py
+++ youtube/download_and_analyze.py
@@ -9,7 +9,7 @@ import cv2
 import base64
 from datetime import datetime
 from openai import OpenAI
-from credentials import telegram_bot_token_btc, telegram_channel_id, openai_api_key, arzineh_channel_id, altcoin_daily_channel_id, crypto_rover_channel_id, glassnode_channel_id, crypto_bureau_channel_id, crypto_jebb_channel_id, michael_wrubel_channel_id, arzineh_plus_channel_id
+from credentials import telegram_bot_token_btc, telegram_channel_id, openai_api_key, arzineh_channel_id, altcoin_daily_channel_id, crypto_rover_channel_id, glassnode_channel_id, crypto_bureau_channel_id, crypto_jebb_channel_id, michael_wrubel_channel_id, arzineh_plus_channel_id, more_crypto_online_channel_id, bitboy_channel_id, bitcoin999_channel_id, cryptorus_channel_id, ivan_on_tech_channel_id
 from youtube_rss import YoutubeFeedParser
 
 telegram_token = telegram_bot_token_btc
@@ -176,7 +176,12 @@ async def main():
         crypto_bureau_channel_id,
         glassnode_channel_id,
         michael_wrubel_channel_id,
-        crypto_jebb_channel_id
+        crypto_jebb_channel_id,
+        more_crypto_online_channel_id,
+        bitboy_channel_id,
+        bitcoin999_channel_id,
+        cryptorus_channel_id,
+        ivan_on_tech_channel_id
     ] 
 
     feeds = [ YoutubeFeedParser(channel_id) for channel_id in channel_ids ]
